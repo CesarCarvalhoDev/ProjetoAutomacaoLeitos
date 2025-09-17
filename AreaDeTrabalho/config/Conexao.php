1fa2543ced5ae625bd0235ee1aa6df5ca2f0eb84
CesarA34R <cesaraugustocarvalhodev@gmail.com>
2025-09-17 20:40:08 -0300

    update config projeto

--- AreaDeTrabalho/config/Conexao.php
+++ AreaDeTrabalho/config/Conexao.php
@@ -1,33 +1,35 @@
 <?php
+require_once __DIR__ . '../../core/EnvLoader.php';
+EnvLoader::Load(__DIR__ . '../../../.env');
+
 class Conexao
 {
-private static $host = "localhost";
-private static $user = "CESARADM";
-private static $password =  "rasen1063";
-private static $database  = "teste_automacao";
-public static $conn;
+    private static $conn;
 
-public static function ConexaoBancoDeDados()
-{
-    if(self::$conn === null)
+    public static function ConexaoBancoDeDados()
     {
-        self::$conn = new mysqli(self::$host,self::$user,self::$password, self::$database);
+        if (self::$conn === null) {
+            // Pega os dados do .env na hora da conexão
+            $host = $_ENV["DB_HOST"];
+            $user = $_ENV['DB_USER'];
+            $password = $_ENV['DB_PASS'];
+            $database = $_ENV['DB_DATABASE'];
 
-        if(self::$conn->connect_error){
-            die("Erro ao connectar ao banco de dados");
-            
+            self::$conn = new mysqli($host, $user, $password, $database);
+
+            if (self::$conn->connect_error) {
+                die("Erro ao conectar ao banco de dados: " . self::$conn->connect_error);
+            }
+        }
+
+        return self::$conn;
+    }
+    public static function FecharConexao()
+    {
+        if (self::$conn !== null) {
+            self::$conn->close();
+            self::$conn = null; // Reinicia a conexão
         }
     }
-    return self::$conn;
-}
-
-public static function FecharConexao()
-{
-    if(self::$conn != null)
-    {
-        self::$conn = self::$conn -> close();
-    }
-}
-
 }
 ?>
