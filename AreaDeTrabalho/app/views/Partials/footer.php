be4e01aef2c99f44dd3f79454c7c758fbded0aa5
CesarA34R <cesaraugustocarvalhodev@gmail.com>
2025-09-19 20:04:07 -0300

    Updatee Project

---
+++ AreaDeTrabalho/app/views/Partials/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer_infos">
+    <?echo(date("yyyy") . "<small>Todos Direitos Reservados</small>)" )?>
+    </div>
+</footer>
---
+++ AreaDeTrabalho/app/views/Partials/header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="logo">
+        <img src="" alt="">
+    </div>
+    <form action="" method="get">
+        <input type="text" name="termo_pesquisa" class="btn-lg-normal" placeholder="Pesquisar">
+        <button type="submit" name="btn-submit" id="btn-buscar" aria-label="Buscar">
+            <img src="" alt="" class="icon" id="lupa">
+        </button>
+    </form>
+</header>
