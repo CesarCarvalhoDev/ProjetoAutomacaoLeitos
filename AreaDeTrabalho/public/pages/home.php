897963c24404117fca7cd2a5166195cbfebd4cc3
CesarA34R <cesaraugustocarvalhodev@gmail.com>
2025-09-27 14:05:11 -0300

    Create home.php

---
+++ AreaDeTrabalho/public/pages/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Página Inicial</title>
+</head>
+<body>
+    <h1>Bem-vindo à Home!</h1>
+    <p>Este é o conteúdo da página inicial, roteado pelo index.php.</p>
+    <p>Visite a página <a href="/sobre">Sobre</a> ou <a href="/contato">Contato</a>.</p>
+</body>
+</html>
