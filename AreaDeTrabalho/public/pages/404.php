564681d9f97f83ea511c6d0a194fdbbd423ee356
CesarA34R <cesaraugustocarvalhodev@gmail.com>
2025-09-27 14:05:14 -0300

    Create 404.php

---
+++ AreaDeTrabalho/public/pages/404.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>404 - Página Não Encontrada</title>
+</head>
+<body>
+    <h1>Erro 404 - Página Não Encontrada</h1>
+    <p>A página que você está procurando não existe. Tente voltar para a <a href="/">página inicial</a>.</p>
+</body>
+</html>
