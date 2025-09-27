89e37d52503dcfffcb220578be749ebf33c30941
CesarA34R <cesaraugustocarvalhodev@gmail.com>
2025-09-27 14:05:02 -0300

    Update Conexao.php

--- AreaDeTrabalho/config/Conexao.php
+++ AreaDeTrabalho/config/Conexao.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../core/EnvLoader.php';
+require_once __DIR__ . '/EnvLoader.php'; // Corrija esse caminho conforme necessário
 EnvLoader::Load(__DIR__ . '/../../.env');
 
 class Conexao
@@ -9,15 +9,15 @@ class Conexao
     public static function ConexaoBancoDeDados()
     {
         if (self::$conn === null) {
-            $host = $_ENV["DB_HOST"];
-            $user = $_ENV['DB_USER'];
-            $password = $_ENV['DB_PASS'];
-            $database = $_ENV['DB_DATABASE'];
+            $host = getenv("DB_HOST");
+            $user = getenv("DB_USER");
+            $password = getenv("DB_PASS");
+            $database = getenv("DB_DATABASE");
 
             self::$conn = new mysqli($host, $user, $password, $database);
 
             if (self::$conn->connect_error) {
-                die("Erro ao conectar ao banco de dados: " . self::$conn->connect_error);
+                throw new Exception("Erro ao conectar ao banco de dados: " . self::$conn->connect_error);
             }
 
             self::$conn->set_charset("utf8mb4");
