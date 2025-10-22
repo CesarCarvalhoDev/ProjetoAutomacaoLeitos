53d46cf75c582d3c913ff517e89b572914584540
CesarA34R <cesaraugustocarvalhodev@gmail.com>
2025-10-22 20:24:02 -0300

    Correções Paciente

--- AreaDeTrabalho/app/models/Paciente.php
+++ AreaDeTrabalho/app/models/Paciente.php
@@ -9,18 +9,9 @@ class Paciente
         $this->conn = Conexao::ConexaoBancoDeDados();
     }
 
-    public function Cadastrar($cpf,$nome,$sexo,$idade,$num_leito,$cracha_medico)
+    public function Cadastrar($cpf,$nome,$sexo,$idade,$id_leito,$id_medico_resp)
     {
-        $sql = "INSERT INTO pacientes(cpf,nome,sexo,idade,num_leito,cracha) VALUES(:cpf,:nome,:sexo,:idade,:num_leito,:cracha_medico)";
-        $stmt = $this->$conn->prepare($sql);
-        $stmt->bindValue(":cpf", $cpf);
-        $stmt->bindValue(":nome", $nome);
-        $stmt->bindValue(":sexo", $sexo);
-        $stmt->bindValue(":idade", $idade);
-        $stmt->bindValue(":num_value", $num_leito);
-        $stmt->bindValue(":cracha_medico", $cracha_medico);
-
-        return $stmt->execute();
+        $sql = "INSERT INTO pacientes ('cpf','sexo','idade','')" 
     }
 }
 
