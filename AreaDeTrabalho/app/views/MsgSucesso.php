dbf269dc66321665696e9e0cc929c66df9740dc6
Artthur2006 <arthursouza683107@gmail.com>
2025-11-15 01:52:19 -0300

    Create Mensagem
    Foi criada a tela de mensagem e sucesso!

---
+++ AreaDeTrabalho/app/views/MsgSucesso.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="refresh" content="2;url=/Home/">
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+        crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="statics/css/style.css" />
+    <title>Sucesso</title>
+</head>
+
+<body class="bg-light">
+
+    <main class="container my-5">
+
+        <div class="row mb-4">
+            <div class="col-12" id="bem_vindo">
+                <div class="border-5 mb-5 my-5">
+                    <h1 class="text-primary text-center">
+                        Cadastro realizado com sucesso! <span class="fw-bold"></span>
+                    </h1>
+                </div>
+                <div>
+                    <img src="\pngwing.com.png" alt="success" width="300px" class="d-block mx-auto">
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
